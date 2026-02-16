c04111bce41d58334852e9033ba1993fbd1a8d69
jcraffael <chao.jiang0517@gmail.com>
2026-02-16 13:42:29 +0100

    New commit of assignment3 used

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 785781c
+AESD_ASSIGNMENTS_VERSION = 11e6517
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
